f91d1862ca03c663b48edae36ba7faf5b7f89d85
kamo72 <agag3932@gmail.com>
2024-05-03 16:15:41 +0900

    adjust somethings

--- src/main/java/com/withReact/sandr/TestController.java
+++ src/main/java/com/withReact/sandr/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-	@GetMapping("/*")
+	@GetMapping("/")
 	public String index()
 	{
 		return "../static/index";
